b386a3630d94ad85e67a3e730c22b5c25ccb26bb
hugosmoreira <hugo.n4@gmail.com>
2021-07-19 23:27:50 -0700

    Front end todolist part 1

---
+++ src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default (props) => (
+  <div>
+    <PageHeader name="Sobre" small="Nos"></PageHeader>
+    <h2>Nossa Historia</h2>
+    <p>Missao do lorem ipsum</p>
+    <h2>Missao e Visao</h2>
+    <p>Outro texto de lorem ipsum</p>
+    <h2>Imprensa</h2>
+    <p>Um texto qualquer sobre imprensa</p>
+  </div>
+);
